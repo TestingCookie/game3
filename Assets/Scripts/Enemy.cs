408728d265025d57b6759bcfe89199c7a5204625
TestingCookie <80402325+TestingCookie@users.noreply.github.com>
2021-03-11 18:59:10 +0100

    poprawki

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,23 +8,29 @@ public class Enemy : MonoBehaviour
     protected Rigidbody2D rb;
     protected AudioSource boomDeath;
 
+    public GameObject makegem;
+
     protected virtual void Start()
     {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         boomDeath = GetComponent<AudioSource>();
+
+
     }
     public void Killed()
     {
         anim.SetTrigger("death");
         boomDeath.Play();
         rb.velocity = Vector2.zero;
-        
+        rb.bodyType = RigidbodyType2D.Kinematic;
+        GetComponent<Collider2D>().enabled = false;
     }
 
 
     private void Death()
     {
         Destroy(this.gameObject);
+        Instantiate(makegem, new Vector2(0, 0), Quaternion.identity);
     }
 }
