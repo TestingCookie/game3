b2c8b9898eae3cf5b1347f74485b67c432d415bc
TestingCookie <80402325+TestingCookie@users.noreply.github.com>
2021-06-06 01:01:57 +0200

    platform fix

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public Transform positionUp, positionDown;
+    public float speed;
+    public Transform startPosition;
+    public bool test = false;
+
+    Vector3 nextPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextPosition = startPosition.position;
+        
+        
+        
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject crank = GameObject.Find("crank-down");
+        TurnOnCrank turnOnCrank = crank.GetComponent<TurnOnCrank>();
+
+        
+        if (turnOnCrank.triggered == true)
+        {
+            MovePlatform();
+        }
+    }
+
+    private void MovePlatform()
+    {
+        if (transform.position == positionUp.position)
+        {
+            nextPosition = positionDown.position;
+        }
+        if (transform.position == positionDown.position)
+        {
+            nextPosition = positionUp.position;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, nextPosition, speed * Time.deltaTime);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(positionUp.position, positionDown.position);
+    }
+}
---
+++ Assets/Scripts/TurnOnCrank.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOnCrank : MonoBehaviour
+{
+    public bool triggered = false;
+    private SpriteRenderer sprite;
+    private Sprite spriteChanged;
+
+    private void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        spriteChanged = Resources.Load<Sprite>("crank-up");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D touchPlayer)
+    {
+        if (touchPlayer.gameObject.tag == "Player")
+        {
+            triggered = true;
+            sprite.sprite = spriteChanged;
+        }
+    }
+
+}
